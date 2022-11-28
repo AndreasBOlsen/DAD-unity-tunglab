63cc4284cda0e44e027edf994e9c9d35a69ed5ad
AndreasBOlsen <77485482+AndreasBOlsen@users.noreply.github.com>
2022-11-28 09:18:58 +0100

    28.11.2022 - 09:15

---
+++ Unity Tunglab/Assets/UI/PopUp/Scripts/MeasurementPopUp.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Security.Policy;
+using TMPro;
+using UnityEngine.UI;
+
+
+
+
+public class MeasurementPopUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject PopUp;
+    public GameObject TMP_Tagname;
+    public GameObject TMP_Measurement;
+    public string phpURL;
+
+
+    //Text components
+    TextMeshProUGUI TagnameText;
+    TextMeshProUGUI MeasurementText;
+
+    //Private variables
+    private string url;
+
+
+
+    void Start()
+    {
+        //url = "http://
+        PopUp.SetActive(false);
+        TagnameText = TMP_Tagname.GetComponent<TextMeshProUGUI>();
+        MeasurementText = TMP_Measurement.GetComponent<TextMeshProUGUI>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    { 
+        PopUp.SetActive(true);
+
+        TagnameText.text = name;
+
+        url = phpURL + "?tagname=" + name + "&amount=1";
+
+        MeasurementText.text = GetMeasurementsFromDataBase();
+
+    }
+
+    void OnTriggerExit(Collider other)
+    { 
+        PopUp.SetActive(false); 
+    }
+    
+    private string GetMeasurementsFromDataBase()
+    {
+        string response;
+        using (WebClient client = new WebClient())
+        {
+            response = client.DownloadString(url);
+        }
+        string[] parts = response.Split(',');
+        return parts[1];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
